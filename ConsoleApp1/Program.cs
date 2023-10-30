bf1e84d3813914d2ffe86f9eeb745b00e25beec8
Edgaras <Str4iker@gmail.com>
2023-10-30 21:03:31 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       *******\\");
+            Console.WriteLine("      **      **\\");
+            Console.WriteLine("    **          **\\");
+            Console.WriteLine("   **     Hello   **\\");
+            Console.WriteLine("   **      from  **\\");
+            Console.WriteLine("    **    C#    **\\");
+            Console.WriteLine("      **       **\\");
+            Console.WriteLine("        *******");
+
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("    /\\");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("   /  \\");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("  /    \\");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(" /______\\");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" / \\     /\\");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("/   \\   /  \\");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\\   \\ /    /");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" \\         /");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("   \\     /");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("     \\  /");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("      \\/");
+
+
+            string preke = "Obuolys";
+            int kiekis = 5;
+            int kaina = 2;
+
+            Console.WriteLine(preke);
+            Console.WriteLine(kiekis);
+            Console.WriteLine(kaina + "euro");
+
+            string vardas = "Vardenis Pavardenis";
+            int amzius = 28;
+            string pareigos = "Software Developer";
+            string email = "johndoe@example.com";
+            int phononumber = +1123-456-7890;
+
+            Console.WriteLine(vardas);
+            Console.WriteLine(amzius);
+            Console.WriteLine(pareigos);
+            Console.WriteLine(email);
+            Console.WriteLine(phononumber);
+
+
+
+
+
+
+
+
+
+
+            
+
+        }
+    }
+}
